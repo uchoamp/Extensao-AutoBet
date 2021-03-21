d7df617c4696c63574909ef12dd9151d1d04a308
Azurique 23 <azurique10111@gmail.com>
2021-03-21 18:56:23 -0300

    Melhorando a parada

--- inject.js
+++ inject.js
@@ -1,26 +1,9 @@
 
-
-var saldo = document.querySelector(".ssc-wla").innerText;
-saldo = saldo.replace("$", "");
-
-var saldoFinal = saldo - quantBet;
-
-
-function chama() {
-    saldo = document.querySelector(".ssc-wla").innerText;
-    saldo = saldo.replace("$", "");
-
-    if (saldo <= saldoFinal) {
-        clearInterval(interval);
-    }
-}
-
-
 var interval = setInterval(() => {
-    chama();
+
     let odd = document.getElementById("mbet-FOURFOLD-1-real-tdo");
 
-    if (odd ) {
+    if (odd) {
         odd = odd.value;
         if (odd > 1.5) {
             document.querySelector("#mbet-FOURFOLD-1-real-stake").value = "0.01";
@@ -31,7 +14,13 @@ var interval = setInterval(() => {
     else {
         let repete = document.querySelector(".add-previous-selections");
         if (repete) {
-            repete.click();
+            quantBet -= 1;
+            if (quantBet == 0) {
+                clearInterval(interval);
+            }else{
+                repete.click();
+            }
+            
         }
     }
 
--- popup.js
+++ popup.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let quantBet = inputQuantBets.value;
 
-    quantBet = quantBet/100
 
     chrome.tabs.executeScript({
       code: "var quantBet = "+quantBet+";"
